d649120bc6ad74bd314686fdca4e8f203b9c3bde
Sprint <sagarwal@ukexportfinance.gov.uk>
2019-11-05 16:05:47 +0000

    first-commit

---
+++ app/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+
+router.post('/juggling-balls-answer',function(req,res){
+    var jugglingBalls = req.session.data['how-many-balls']
+    if (jugglingBalls == "3 or more"){
+       
+        res.redirect('/juggling-trick')
+    }
+    else{
+        res.redirect('/ineligible')
+    }
+})
+
+module.exports = router
+
+
